0836a5759a314a89e29d629f8c3552530b217852
thanhbui27 <bthanh2001@gmail.com>
2025-04-01 11:51:53 +0700

    [ThanhBN]execrise 3

--- src/main/java/bap/jp/thanhbn/web_mvc/repository/OrderRepository.java
+++ src/main/java/bap/jp/thanhbn/web_mvc/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package bap.jp.thanhbn.web_mvc.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,14 @@ import bap.jp.thanhbn.web_mvc.model.Order;
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
 	List<Order> findByUser_userID(int userId);
+	
+	@Query(value = "select \r\n"
+			+ "case \r\n"
+			+ "	   when sum(total_amount) = 0 then 0\r\n"
+			+ "    when sum(total_amount) > 0 then sum(total_amount)\r\n"
+			+ "end as total_amount\r\n"
+			+ "from orders \r\n"
+			+ "where month(CURRENT_DATE()) = month(order_date)",
+			nativeQuery = true)
+	Optional<Integer> getTotalAmountInMonth();
 }
